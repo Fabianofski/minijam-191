e8ecbd3f2954f290f499d58b53f6d3919266fc99
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-08-15 21:19:05 +0200

    Add: decoration and obstacle spawner

---
+++ scenes/game/obstacles/obstacle_res.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Obstacle
+
+@export var Prefab: PackedScene 
+@export var Min_spawn_delay: float 
+@export var Max_spawn_delay: float
--- scenes/game/obstacles/obstacle_spawner.gd
+++ scenes/game/obstacles/obstacle_spawner.gd
@@ -1,33 +1,33 @@
 extends Node2D
 
-@onready var obstacle = load("res://scenes/game/obstacles/obstacle.tscn")
 @export var bounds_x: float
 @export var slots: int = 12
-@export var min_spawn_delay: float = 1.0
-@export var max_spawn_delay: float = 3.0
+@export var obstacles: Array[Obstacle]
 
 var rng = RandomNumberGenerator.new()
 var positions: Array = []
-var spawn_timer: Timer
 
 func _ready() -> void:
 	rng.randomize()
 	refill_positions()
-	spawn_timer = Timer.new()
-	spawn_timer.one_shot = true
-	add_child(spawn_timer)
-	spawn_timer.timeout.connect(spawn_new_obstacle)
-	start_spawn_timer()
+	for obstacle in obstacles:
+		var spawn_timer = Timer.new()
+		spawn_timer.one_shot = true
+		add_child(spawn_timer)
+		spawn_timer.timeout.connect(func(): spawn_new_obstacle(obstacle, spawn_timer))
+		start_spawn_timer(obstacle, spawn_timer)
 
-func spawn_new_obstacle() -> void:
+func spawn_new_obstacle(obstacle: Obstacle, spawn_timer: Timer) -> void:
 	if positions.is_empty():
 		refill_positions()
-	var obs = obstacle.instantiate()
+	var obs = obstacle.Prefab.instantiate()
 	obs.position.x = positions.pop_front()
 	add_child(obs)
-	start_spawn_timer()
+	start_spawn_timer(obstacle, spawn_timer)
 
-func start_spawn_timer() -> void:
+func start_spawn_timer(obstacle: Obstacle, spawn_timer: Timer) -> void:
+	var min_spawn_delay = obstacle.Min_spawn_delay
+	var max_spawn_delay = obstacle.Max_spawn_delay
 	spawn_timer.wait_time = rng.randf_range(min_spawn_delay, max_spawn_delay)
 	spawn_timer.start()
 
