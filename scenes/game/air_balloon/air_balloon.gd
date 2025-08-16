bf9eb223cc01fa3ce6713936d170a1f38abda5bf
Santum <39064333+santumerino@users.noreply.github.com>
2025-08-15 21:15:59 -0300

    add: Balloon rotates in the direction of wind

--- scenes/game/air_balloon/air_balloon.gd
+++ scenes/game/air_balloon/air_balloon.gd
@@ -6,6 +6,8 @@ var diff: Vector2
 @export var bounds: float
 @export var speed: float
 
+@onready var balloon_graphics: Node2D = $balloon
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouse_pos = get_global_mouse_position()
@@ -15,6 +17,8 @@ func _process(delta: float) -> void:
 	var blow_strength = MicControl.get_blow_strength()
 	position.x -= diff.x * speed * delta * blow_strength
 	position.x = clamp(position.x, -bounds, bounds)
+	# Balloon rotation
+	balloon_graphics.look_at((get_local_mouse_position().rotated(PI/2))*-1)
 
 func _on_body_entered(_body: Node2D) -> void:
 	SignalBus.game_over.emit()
