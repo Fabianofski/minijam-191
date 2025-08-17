8d98f62d8ab6b85b7167c96a5156f7300a5ba0f4
Santum <39064333+santumerino@users.noreply.github.com>
2025-08-17 13:57:22 -0300

    change: Lose screen (currently broken)

--- scenes/game/lose_screen/lose.gd
+++ scenes/game/lose_screen/lose.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@onready var label: Label = $Label
+@onready var high_score: Label = $"High Score"
+
 func _init():
 	SignalBus.game_over.connect(game_over)
 
 func game_over(): 
 	GameManager.reset()
+	label.text = "You fell for "+str(int(GameManager.distance_travelled))+" metres!"
+	high_score.text = "And yes! It's a high score." if GameManager.distance_travelled >= GameManager.highscore else "(And your high score is "+str(int(GameManager.highscore))+" metres.)"
 	self.visible = true
 
 func restart(): 
